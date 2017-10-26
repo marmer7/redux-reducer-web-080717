a56b0f37a85bfb0fb1576b1e5e394ef96b4e1556
Marlon Merjos <marlonmerjos@gmail.com>
2017-10-26 10:27:38 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      let friends = state.friends.slice(0);
+      friends.push(action.friend);
+      return { friends };
+    case "REMOVE_FRIEND":
+      friends = state.friends.filter(friend => friend.id !== action.id);
+      return { friends };
+    default:
+      return state;
+  }
 }
